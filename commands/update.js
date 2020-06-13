19a563fdc2251c9018eca351933719fc8b5b7053
Daniel Walder <daniel.walder@gmail.com>
2020-06-13 23:46:34 +1000

    Make sure the update command posts the update message before updating

--- commands/update.js
+++ commands/update.js
@@ -10,13 +10,13 @@ module.exports = {
      * @param {Discord.Message} message 
      * @param {Array<string>} args 
      */
-    execute(message, args) {
+    async execute(message, args) {
         if (message.channel.name !== 'bot-testing-channel') return;
         if (!lib.memberHasAllRoles(message.member, ['Admins'])) return;
         if (message.member.id !== '130279066451312640') return; // Go away everyone else
 
         const production = message.guild.id === '291678871856742400';
-        message.channel.send('Updating...');
+        await message.channel.send('Updating...');
 
         if (production) {
             child_process.execSync('git reset --hard');
